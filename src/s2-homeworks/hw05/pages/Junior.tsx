c540f945619c1e82a50cb0c91c3dfbee69563b2b
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-02-15 13:19:31 +0100

    hw8 main part complete

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HW6 from "s2-homeworks/hw06/HW6";
 import HW7 from "s2-homeworks/hw07/HW7";
+import HW8 from "s2-homeworks/hw08/HW8";
 
 function Junior() {
     return (
         <div id={'hw5-page-junior'}>
             <HW6 />
             <HW7 />
-            {/*<HW8 />*/}
+            <HW8 />
             {/*<HW9 />*/}
         </div>
     )
