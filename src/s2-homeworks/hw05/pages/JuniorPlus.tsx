51cc74618476d5d3f3760d0cf402198b1c7bc823
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-03-08 15:48:59 +0100

    hw11 main part complete

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HW10 from 's2-homeworks/hw10/HW10'
+import HW11 from "s2-homeworks/hw11/HW11";
 
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
             <HW10 />
-            {/*<HW11 />*/}
+            <HW11 />
             {/*<HW12 />*/}
             {/*<HW13 />*/}
             {/*<HW14 />*/}
