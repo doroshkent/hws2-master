f3c92661913e94af2837acace8438aff9551e85f
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-03-13 21:49:03 +0100

    hw12 main part complete

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HW10 from 's2-homeworks/hw10/HW10'
 import HW11 from "s2-homeworks/hw11/HW11";
+import HW12 from "s2-homeworks/hw12/HW12";
 
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
             <HW10 />
             <HW11 />
-            {/*<HW12 />*/}
+            <HW12 />
             {/*<HW13 />*/}
             {/*<HW14 />*/}
             {/*<HW15 />*/}
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,18 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: ThemeState = initState, action: ThemeActions): ThemeState => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) as const
+
+// types
+export type ThemeState = typeof initState
+type ThemeActions = ReturnType<typeof changeThemeId>
