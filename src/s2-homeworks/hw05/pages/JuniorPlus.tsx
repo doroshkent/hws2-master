7604b67c68d4f132e4cd1274a2a6ba2c897ac3cd
doroshkent <108677278+doroshkent@users.noreply.github.com>
2024-04-30 16:39:49 +0200

    hw15 main part complete

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -4,6 +4,7 @@ import HW11 from "s2-homeworks/hw11/HW11";
 import HW12 from "s2-homeworks/hw12/HW12";
 import HW13 from "s2-homeworks/hw13/HW13";
 import HW14 from "s2-homeworks/hw14/HW14";
+import HW15 from "s2-homeworks/hw15/HW15";
 
 function JuniorPlus() {
     return (
@@ -13,7 +14,7 @@ function JuniorPlus() {
             <HW12 />
             <HW13 />
             <HW14 />
-            {/*<HW15 />*/}
+            <HW15 />
         </div>
     )
 }
--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
-import {Pagination} from '@mui/material'
+import { Pagination } from '@mui/material'
 import s from './SuperPagination.module.css'
 
 export type SuperPaginationPropsType = {
@@ -16,14 +16,14 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = (
         page, itemsCountForPage, totalCount, onChange, id = 'hw15',
     }
 ) => {
-    const lastPage = 10 // пишет студент // вычислить количество страниц
+    const lastPage = Math.ceil(totalCount / itemsCountForPage)
 
     const onChangeCallback = (event: any, page: number) => {
-        // пишет студент
+      onChange(page, itemsCountForPage)
     }
 
-    const onChangeSelect = (event: any) => {
-        // пишет студент
+    const onChangeSelect = (value: string) => {
+      onChange(1, +value)
     }
 
     return (
@@ -52,7 +52,7 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = (
                     {id: 7, value: 7},
                     {id: 10, value: 10},
                 ]}
-                onChange={onChangeSelect}
+                onChangeOption={onChangeSelect}
             />
 
             <span className={s.text2}>
